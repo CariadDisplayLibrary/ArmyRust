af7792bb2620d749d381897b36c9932bad9134a2
Matt Jenkins <matt@majenko.co.uk>
2017-09-03 00:35:09 +0100

    Initial import

---
+++ ArmyRust.h
@@ -0,0 +1,29 @@
+#ifndef _ARMYRUST_H
+#define _ARMYRUST_H
+
+#include <DisplayCore.h>
+
+namespace Fonts {
+    extern const uint8_t ArmyRust10[];
+    extern const uint8_t ArmyRust11[];
+    extern const uint8_t ArmyRust12[];
+    extern const uint8_t ArmyRust13[];
+    extern const uint8_t ArmyRust14[];
+    extern const uint8_t ArmyRust15[];
+    extern const uint8_t ArmyRust16[];
+    extern const uint8_t ArmyRust17[];
+    extern const uint8_t ArmyRust18[];
+    extern const uint8_t ArmyRust19[];
+    extern const uint8_t ArmyRust20[];
+    extern const uint8_t ArmyRust21[];
+    extern const uint8_t ArmyRust22[];
+    extern const uint8_t ArmyRust23[];
+    extern const uint8_t ArmyRust24[];
+    extern const uint8_t ArmyRust25[];
+    extern const uint8_t ArmyRust26[];
+    extern const uint8_t ArmyRust27[];
+    extern const uint8_t ArmyRust28[];
+    extern const uint8_t ArmyRust29[];
+    extern const uint8_t ArmyRust30[];
+};
+#endif
